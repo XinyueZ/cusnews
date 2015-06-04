e9eea11eb6968148b431ed8f618f8d2b40d49fcb
XinyueZhao <zhao@eosnewmedia.de>
2015-06-04 14:52:41 +0200

    Add key.properties for dev and live flover.

---
+++ Cus!News/app/src/main/java/com/cusnews/ds/Entry.java
@@ -0,0 +1,20 @@
+package com.cusnews.ds;
+
+
+import java.util.List;
+
+public final class Entry {
+	//"title": "Formartti Gold iPhone Case Looks Fit For Royalty",
+	//		"kwic": "If there’s one complaint many people have had with regards to the iPhone is that it’s too expensive, at least compared to the competition out ...",
+	// "content": "",
+		//	"url": "http://www.ubergizmo.com/2015/06/formartti-gold-iphone-case/",
+		//	"iurl": "http://cdn2.ubergizmo.com/wp-content/uploads/2015/06/formartti-640x422.jpg",
+		//	"domain": "www.ubergizmo.com",
+		//	"author": "Tyler Lee",
+		//	"news": true,
+		//	"votes": "20",
+		//	"date": 1433418207000,
+	private List<Entry> mRelated;
+
+
+}
